876cf6b4615ac4ae810dbb1d3d1ea22edb498f44
xeceryn <adityamusthofa67@gmail.com>
2021-03-14 00:06:19 +0700

    rounded menu

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Image, FlatList, Dimensions, ImageBackground, TouchableOpacity } from 'react-native';
-import { Container, Header, Card, Text, Button, Icon, View, Left, Right, Body, Title } from 'native-base';
+import { Image, FlatList, Dimensions, TouchableWithoutFeedback } from 'react-native';
+import { Container, Card, Text, View } from 'native-base';
 
 const homeMenu = [
   {
@@ -51,35 +51,44 @@ export default class Home extends Component {
   render() {
     return (
       <Container>
-        {/* <Header style={{ backgroundColor: '#2faaff' }}>
-            <Left>
-               <Button transparent  onPress={()=>this.props.navigation.openDrawer()}>
-               <Image source={require('../drawable/list.png')} style={{ width: 25, height: 25}} />
-               </Button>
-            </Left>
-            <Body>
-               <Image source={require('../drawable/title_jagawarga.png')} style={{ width: 300, height: 25}} ></Image>
-            </Body>
-            <Right>
-               <Button transparent onPress={()=> this.props.navigation.navigate("Login")}>
-               <Image source={require('../drawable/loginicon.png')} style={{ width: 25, height: 25}} />
-               </Button>
-            </Right>
-         </Header> */}
         <FlatList
           data={homeMenu}
           renderItem={({ item, index }) => (
-            <TouchableOpacity onPress={() => this.props.navigation.navigate(item.pres)}>
-              <Card style={{ width: Dimensions.get('screen').width / 3.2, height: Dimensions.get('screen').height / 7, marginLeft: 5.5 }} onPress={item.url}>
-                <View style={{ alignItems: 'center', padding: 10 }}>
-                  <Image source={item.image} style={{ width: 75, height: 75, resizeMode: 'contain' }} />
-                  <Text>{item.nama}</Text>
+            <TouchableWithoutFeedback
+              onPress={() => this.props.navigation.navigate(item.pres)}
+            >
+              <Card style={{
+                width: Dimensions.get('screen').width / 4,
+                height: Dimensions.get('screen').height / 8.3,
+                borderRadius: 20,
+                marginLeft: 10,
+                marginRight: 10
+              }}
+                onPress={item.url}
+              >
+                <View style={{
+                  alignItems: 'center',
+                  padding: 10
+                }}>
+                  <Image
+                    source={item.image}
+                    style={{
+                      width: 65,
+                      height: 65,
+                      resizeMode: 'contain'
+                    }} />
+                  <Text
+                    style={{
+                      color: '#000',
+                      fontWeight: 'bold'
+                    }}>{item.nama}</Text>
                 </View>
               </Card>
-            </TouchableOpacity>
+            </TouchableWithoutFeedback>
           )}
           numColumns={3}
           keyExtractor={item => item.id}
+          style={{ alignSelf: 'center', margin: 20 }}
         />
       </Container>
     );
