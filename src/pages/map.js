b0430c586bbc05d1506c970d2430b55fe9b51bcb
xeceryn <adityamusthofa67@gmail.com>
2021-03-30 21:09:16 +0700

    done add mapview

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,30 +1,13 @@
 import * as React from 'react';
-import { View, Text, Image} from 'react-native';
-import { Container, Header, Button, Right} from 'native-base';
+import { WebView } from 'react-native-webview';
 
 
 const MapScreen = ({ navigation }) => {
-    return (
-       <Container>
-         <Header style={{ backgroundColor: '#2faaff' }}>
-            {/* <Left>
-               <Button transparent  onPress={()=>this.props.navigation.openDrawer()}>
-               <Image source={require('../drawable/list.png')} style={{ width: 25, height: 25}} />
-               </Button>
-            </Left>
-            <Body>
-               <Image source={require('../drawable/title_jagawarga.png')} style={{ width: 300, height: 25}} ></Image>
-            </Body>
-            <Right>
-               <Button transparent onPress={()=> this.props.navigation.navigate("Login")}>
-               <Image source={require('../drawable/loginicon.png')} style={{ width: 25, height: 25}} />
-               </Button>
-            </Right> */}
-         </Header>
-         <View>
-         <Text>Ini halaman map</Text>
-         </View>
-      </Container>
-    );
-  }
-  export default MapScreen
+   return (
+      <WebView
+         source={{ uri: 'https://xeceryn.github.io/leafletjs-map/' }}
+      // style={{ width: 100, height: 500 }}
+      />
+   );
+}
+export default MapScreen
