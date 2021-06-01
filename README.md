709959ae7f9d571eeca05a6663de63caab8996a6
Dicky Ganda <dickyganda@gmail.com>
2021-06-01 21:44:47 +0700

    src/pages/pengingat.js

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 ## TODO ?
 1. Rapikan Halaman Drawer Navigasi.
-  1. Login button dipindah ke drawer.
-2. Rapikan Halamaan Home/Dashboard.
-  1. Menambahkan Carousel Pada Home/Dashboard.
-  2. Header Sebelah adalah nama user yang login.
-3. Rapikan Halaman Bantuan bagian Form.
-  1. Form Satuan Dropdown select. format { pcs, kg, liter }.
-4. Rapikan Halaman Pengingat.
-  1. Tambahkan Colom status pada listview, ketentuan { 0 = selesai, != 0 = belum selesai }.
-  2. Tambahkan fungsi pencarian.
-5. Rapikan Halaman Login.
+2. Login button dipindah ke drawer.
+3. Rapikan Halamaan Home/Dashboard.
+4. Menambahkan Carousel Pada Home/Dashboard.
+5. Header Sebelah adalah nama user yang login.
+6. Rapikan Halaman Bantuan bagian Form.
+7. Form Satuan Dropdown select. format { pcs, kg, liter }.
+8. Rapikan Halaman Pengingat.
+9. Tambahkan Colom status pada listview, ketentuan { 0 = selesai, != 0 = belum selesai }.
+10. Tampilan Pencarian Pengingat.
+11. Tambahkan fungsi pencarian.
+12. Rapikan Halaman Login.
--- src/pages/pengingat.js
+++ src/pages/pengingat.js
@@ -1,58 +1,57 @@
 import * as React from 'react';
 import { View, Text, TouchableOpacity, TextInput, StyleSheet} from 'react-native';
 import { Container, Header, Left, Button, Icon, Body, Title, Right, Image } from 'native-base';
-import { DataTable } from 'react-native-paper';
+import { Searchbar, DataTable } from 'react-native-paper';
 import axios from 'axios'
 import {BASE_URL,VIEWKARANTINA} from '../api'
 
+const dataPengingat = [
+   {"id": 0, "nama":"DICKY GANDA PRAYOGI","nama_penyakit":"DBD","waktu_karantina":"Selesai","status":"sakit"},
+   {"id": 1,"nama":"KAMAT","nama_penyakit":"-","waktu_karantina":"-","status":"sehat"},
+   {"id": 2,"nama":"LIPA","nama_penyakit":"-","waktu_karantina":"-","status":"sehat"},
+   {"id": 3,"nama":"KAMDANI","nama_penyakit":"-","waktu_karantina":"-","status":"sehat"},
+   {"id": 4,"nama":"MUSTAHAL","nama_penyakit":"-","waktu_karantina":"-","status":"sehat"},
+   {"id": 5,"nama":"SEMAH","nama_penyakit":"-","waktu_karantina":"-","status":"sehat"},
+   {"id": 6,"nama":"BAERAN","nama_penyakit":"-","waktu_karantina":"-","status":"sehat"},
+   {"id": 7,"nama":"MUNARI","nama_penyakit":"-","waktu_karantina":"-","status":"sehat"},
+   {"id": 8,"nama":"TASAM","nama_penyakit":"-","waktu_karantina":"-","status":"sehat"},
+   {"id": 9,"nama":"NURALIM","nama_penyakit":"-","waktu_karantina":"-","status":"sehat"},
+];
 const PengingatScreen = ({ navigation }) => {
    const [data, setData] = React.useState(null);
 
    React.useEffect(() => {
-     axios.get(BASE_URL+VIEWKARANTINA)
-    .then(res => {
-       setData({
-          data : res.data
-       })
-    })
-    .catch(e => {
-       console.log(e)
-    })
+   //   axios.get(BASE_URL+VIEWKARANTINA)
+   //  .then(res => {
+   //     setData({
+   //        data : res.data
+   //     })
+   //  })
+   //  .catch(e => {
+   //     console.log(e)
+   //  })
    });
 
     return (
        <Container>
-         <Header style={{ backgroundColor: '#2faaff' }}>
-            {/* <Left>
-               <Button transparent  onPress={()=>this.props.navigation.openDrawer()}>
-               <Image source={require('../drawable/list.png')} style={{ width: 25, height: 25}} />
-               </Button>
-            </Left>
-            <Body>
-               <Image source={require('../drawable/title_jagawarga.png')} style={{ width: 300, height: 25}} ></Image>
-            </Body>
-            <Right>
-               <Button transparent onPress={()=> this.props.navigation.navigate("Login")}>
-               <Image source={require('../drawable/loginicon.png')} style={{ width: 25, height: 25}} />
-               </Button>
-            </Right> */}
-         </Header>
-      <View style={styles.container}>
-         <Text style = {styles.titletext}>Pengingat</Text>
-   {data === null ? 
+          <Searchbar placeholder="Search" />
+   {dataPengingat === null ? 
+      
       <Text>Tunggu</Text>
    :
     <DataTable>
     <DataTable.Header>
-      <DataTable.Title>Nama</DataTable.Title>
-      <DataTable.Title numeric>Nama Penyakit</DataTable.Title>
-      <DataTable.Title numeric>Sisa Waktu (Hari)</DataTable.Title>
+      <DataTable.Title style={{ flex: 2}}>Nama</DataTable.Title>
+      <DataTable.Title>Nama Penyakit</DataTable.Title>
+      <DataTable.Title>Sisa Waktu (Hari)</DataTable.Title>
+      <DataTable.Title>Status</DataTable.Title>
     </DataTable.Header>
-      {data.data.map((data) =>
-         <DataTable.Row key={data.id_karantina}>
-            <DataTable.Cell>{data.nama}</DataTable.Cell>
-            <DataTable.Cell numeric>{data.nama_penyakit}</DataTable.Cell>
-            <DataTable.Cell numeric>{data.waktu_karantina}</DataTable.Cell>
+      {dataPengingat.map((data) =>
+         <DataTable.Row key={data.id}>
+            <DataTable.Cell style={{ flex: 2}}>{data.nama}</DataTable.Cell>
+            <DataTable.Cell>{data.nama_penyakit}</DataTable.Cell>
+            <DataTable.Cell>{data.waktu_karantina}</DataTable.Cell>
+            <DataTable.Cell>{data.status}</DataTable.Cell>
          </DataTable.Row>
       )}
 
@@ -66,7 +65,6 @@ const PengingatScreen = ({ navigation }) => {
     /> */}
   </DataTable>
    }
-      </View>
       </Container>
     );
   }
