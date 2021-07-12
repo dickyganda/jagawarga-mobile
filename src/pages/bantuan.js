90305b17ad37bfe924f3a6c5b3b853ca2e4af378
xeceryn <adityamusthofa67@gmail.com>
2021-07-12 20:07:29 +0700

    finish fungsi pencarian di halaman pengingat

--- src/pages/bantuan.js
+++ src/pages/bantuan.js
@@ -1,109 +1,115 @@
 import * as React from 'react';
-import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import axios from 'axios';
 
 import { BASE_URL, INSERT } from '../store/typeStore'
 import { Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const BantuanScreen = ({ navigation }) => {
-   const [form, setForm] = React.useState({
-      jenis_bantuan: '',
-      stok: '',
-      satuan: undefined
-   });
-   const satuan = undefined
-   const handleChange = (key, value) => {
-      setForm({
-         ...form,
-         [key]: value
-      })
-   }
+  const [form, setForm] = React.useState({
+    jenis_bantuan: '',
+    stok: '',
+    satuan: undefined,
+  });
+  const [loading, setLoading] = React.useState(false);
+  const satuan = undefined;
+  const handleChange = (key, value) => {
+    setForm({
+      ...form,
+      [key]: value,
+    });
+  };
 
-   const handleSubmit = () => {
-      if (form.jenis_bantuan && form.stok && form.satuan) {
-         const data = {
-            jenis_bantuan: form.jenis_bantuan,
-            stok: form.stok,
-            satuan: form.satuan,
-         }
-         axios.post(BASE_URL + INSERT, data)
-            .then(res => {
-               const response = res.data;
-               if (response.status === "failed") {
-                  Alert.alert(
-                     "Tambah data gagal!",
-                     `${response.status}`
-                  );
-               }
-               else {
-                  Alert.alert(
-                     `${response.reason}`,
-                  );
-               }
-            })
-            .catch(e => {
-               console.log(e)
-            })
+  const handleSubmit = async () => {
+    if (form.jenis_bantuan && form.stok && form.satuan) {
+      const nik = await AsyncStorage.getItem('nik');
+      const data = {
+        jenis_bantuan: form.jenis_bantuan,
+        stok: form.stok,
+        satuan: form.satuan,
+        nik: nik,
+      };
+      setLoading(true);
+      axios
+        .post(BASE_URL + INSERT, data)
+        .then((res) => {
+          const response = res.data;
+          setLoading(false);
+          if (response.status === 'failed') {
+            Alert.alert('Tambah data gagal!', `${response.status}`);
+          } else {
+            Alert.alert(`${response.reason}`);
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    } else {
+      Alert.alert('Input bantuan gagal !', 'Data yang dimasukkan salah');
+      // alert('Username & Password tidak boleh kosong')
+    }
+  };
 
-      } else {
-         Alert.alert(
-            "Input bantuan gagal !",
-            "Data yang dimasukkan salah"
-         );
-         // alert('Username & Password tidak boleh kosong')
-      }
-   }
-
-
-   return (
-      <View style={styles.container}>
-         <View style={styles.inputContainer}>
-            <Text style={styles.titletext}>Jenis Bantuan</Text>
-            <TextInput style={styles.input}
-               underlineColorAndroid="transparent"
-               placeholder="Jenis Bantuan"
-               placeholderTextColor="#008dcb"
-               onChangeText={(value) => handleChange('jenis_bantuan', value)}
-               autoCapitalize="none"
-               keyboardType="default"
-            />
-         </View>
-         <View style={styles.inputContainer}>
-            <Text style={styles.titletext}>Jumlah</Text>
-            <TextInput style={styles.input}
-               underlineColorAndroid="transparent"
-               placeholder="Jumlah"
-               placeholderTextColor="#008dcb"
-               onChangeText={(value) => handleChange('stok', value)}
-               autoCapitalize="none"
-               keyboardType="number-pad"
-            />
-         </View>
-         <View style={styles.inputContainer}>
-            <Text style={styles.titletext}>Satuan</Text>
-            <View style={styles.input}>
-               <Picker
-                  mode="dropdown"
-                  selectedValue={satuan}
-                  onValueChange={(newSatuan) => handleChange('satuan', newSatuan)}
-               >
-                  <Picker.Item color='#008dcb' label="Pcs" value="Pcs" />
-                  <Picker.Item color='#008dcb' label="Kg" value="Kg" />
-                  <Picker.Item color='#008dcb' label="Liter" value="Liter" />
-               </Picker>
-            </View>
-         </View>
-         <TouchableOpacity
-            style={styles.submitButton}
-            onPress={
-               () => handleSubmit()
-            }>
-            <Text style={styles.submitButtonText}> Submit </Text>
-         </TouchableOpacity>
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Text style={styles.titletext}>Jenis Bantuan</Text>
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder="Jenis Bantuan"
+          placeholderTextColor="#008dcb"
+          onChangeText={(value) => handleChange('jenis_bantuan', value)}
+          autoCapitalize="none"
+          keyboardType="default"
+        />
       </View>
-   );
+      <View style={styles.inputContainer}>
+        <Text style={styles.titletext}>Jumlah</Text>
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder="Jumlah"
+          placeholderTextColor="#008dcb"
+          onChangeText={(value) => handleChange('stok', value)}
+          autoCapitalize="none"
+          keyboardType="number-pad"
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.titletext}>Satuan</Text>
+        <View style={styles.input}>
+          <Picker
+            mode="dropdown"
+            selectedValue={satuan}
+            onValueChange={(newSatuan) => handleChange('satuan', newSatuan)}>
+            <Picker.Item color="#008dcb" label="Pcs" value="Pcs" />
+            <Picker.Item color="#008dcb" label="Kg" value="Kg" />
+            <Picker.Item color="#008dcb" label="Liter" value="Liter" />
+          </Picker>
+        </View>
+      </View>
+      <TouchableOpacity
+         style={styles.submitButton}
+         onPress={() => handleSubmit()}>
+         {loading ? (
+            <ActivityIndicator size="large" color="#000" />
+         ) : (
+            <Text style={styles.submitButtonText}> Submit </Text>
+         )}
+      </TouchableOpacity>
+    </View>
+  );
 }
 export default BantuanScreen
 
--- src/pages/pengingat.js
+++ src/pages/pengingat.js
@@ -1,79 +1,70 @@
 import * as React from 'react';
-import { Text, StyleSheet, ScrollView } from 'react-native';
+import { ScrollView, ActivityIndicator } from 'react-native';
 import { Container } from 'native-base';
 import { Searchbar, DataTable } from 'react-native-paper';
 import axios from 'axios';
-import Icon from 'react-native-vector-icons/dist/Ionicons'
 
 import { BASE_URL, VIEWKARANTINA } from '../store/typeStore'
 
 const PengingatScreen = ({ navigation }) => {
-   const [data, setData] = React.useState(null);
-   React.useEffect(() => {
-   axios.get(BASE_URL+VIEWKARANTINA)
-    .then(async res => {
-      await setData(res.data)
-    })
-    .catch(e => {
-       console.log(e)
-    })
-   }, [axios]);
+  const [users, setUser] = React.useState([]);
+  const [loading, setloading] = React.useState(false);
+  const [search, setSearch] = React.useState("");
+  const [filteredUser, setFilteredUser] = React.useState([]);
 
+  React.useEffect(() => {
+    setloading(true);
+    axios
+      .get(BASE_URL + VIEWKARANTINA)
+      .then((res) => {
+        setUser(res.data);
+        setloading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, []);
+
+  React.useEffect(() => {
+    setFilteredUser(
+      users.filter((user) => 
+        user.nama.toLowerCase().includes(search.toLowerCase())
+      )
+    )
+  }, [search, users]);
+
+  if (loading) {
     return (
-       <Container>
-         <Searchbar placeholder="Search" icon={{ uri: 'https://img.icons8.com/search'}} />
-         {data === null ? 
-         <Text style={{ textAlign: 'center', fontWeight: 'bold', marginTop: 2}}>Tunggu</Text> :
-   <ScrollView showsHorizontalScrollIndicator={false}>
-      <DataTable>
-      <DataTable.Header>
-         <DataTable.Title style={{ flex: 2 }}>Nama</DataTable.Title>
-         <DataTable.Title>Nama Penyakit</DataTable.Title>
-         <DataTable.Title>Sisa Waktu (Hari)</DataTable.Title>
-         <DataTable.Title>Status</DataTable.Title>
-      </DataTable.Header>
-         {data.map((data, index) =>
-         <DataTable.Row key={index.toString()}>
-            <DataTable.Cell style={{ flex: 2}}>{data.nama}</DataTable.Cell>
-            <DataTable.Cell>{data.nama_penyakit}</DataTable.Cell>
-            <DataTable.Cell>{data.waktu_karantina}</DataTable.Cell>
-            <DataTable.Cell>{data.status}</DataTable.Cell>
-         </DataTable.Row> 
-         )}
-      </DataTable>
-   </ScrollView>
-   }
+      <ActivityIndicator size="large" color="#000" style={{ flex: 1}} />
+    )
+  }
+
+  return (
+    <Container>
+      <Searchbar
+        placeholder="Search"
+        icon={{uri: 'https://img.icons8.com/search'}}
+        onChangeText={(value) => setSearch(value)}
+      />
+        <ScrollView showsHorizontalScrollIndicator={false}>
+            <DataTable>
+              <DataTable.Header>
+                <DataTable.Title style={{flex: 2}}>Nama</DataTable.Title>
+                <DataTable.Title style={{flex: 2}}>Nama Penyakit</DataTable.Title>
+                <DataTable.Title style={{ flex: 2}}>Sisa Waktu (Hari)</DataTable.Title>
+                <DataTable.Title>Status</DataTable.Title>
+              </DataTable.Header>
+              {filteredUser.map((user, index) => (
+                <DataTable.Row key={index.toString()}>
+                  <DataTable.Cell style={{flex: 2}}>{user.nama}</DataTable.Cell>
+                  <DataTable.Cell style={{flex: 2}}>{user.nama_penyakit}</DataTable.Cell>
+                  <DataTable.Cell style={{ flex: 2}}>{user.waktu_karantina}</DataTable.Cell>
+                  <DataTable.Cell>{user.status}</DataTable.Cell>
+                </DataTable.Row>
+              ))}
+            </DataTable>
+          </ScrollView>
       </Container>
     );
   }
-  export default PengingatScreen
-
-  const styles = StyleSheet.create({
-   container: {
-      paddingTop: 23
-   },
-
-   titletext:{
-      fontSize: 18,
-      color: '#2faaff',
-      textAlign: 'center',
-   },
-
-   input: {
-      margin: 15,
-      height: 40,
-      borderColor: '#008dcb',
-      borderWidth: 1
-   },
-
-   submitButton: {
-      backgroundColor: '#2faaff',
-      padding: 10,
-      margin: 15,
-      height: 40,
-   },
-
-   submitButtonText:{
-      color: 'white'
-   }
-})
+export default PengingatScreen;
